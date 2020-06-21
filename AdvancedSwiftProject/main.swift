57023b25b80f97985474ca50abdd387891c0006e
Yunus MAKAN <makanyunus@gmail.com>
2020-06-21 12:38:26 +0300

    Initial Commit

---
+++ AdvancedSwiftProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvancedSwiftProject
+//
+//  Created by MAKAN on 21.06.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
