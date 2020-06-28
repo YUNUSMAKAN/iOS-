d76380492e7922a55ad0a9decb7a2b8b703d9e16
Yunus MAKAN <makanyunus@gmail.com>
2020-06-28 16:28:31 +0300

    References vs Value Types!

--- AdvancedSwiftProject/main.swift
+++ AdvancedSwiftProject/main.swift
@@ -10,6 +10,48 @@ import Foundation
 
 let classJames = MusicianClass(nameInput: "James", ageInput: 50, instrumentInput: "Guitar")
 
-print(classJames.age)
+//print(classJames.age)
 
-let structJames = MusicianStruct(name: "James", age: 50, instrument: "Guitar")
+var structJames = MusicianStruct(name: "James", age: 50, instrument: "Guitar")
+
+//IMMUTABLE STRUCT
+
+//print(structJames.age)
+
+//Struct lar immutable!
+
+classJames.age = 51
+structJames.age = 51
+
+
+//print(structJames.age)
+
+
+//REFERENCE vs VALUE
+
+let copyOfClassJames = classJames
+
+var copyOfStructJames = structJames
+
+print(copyOfClassJames.age)
+print(copyOfStructJames.age)
+
+
+copyOfStructJames.age = 52
+copyOfClassJames.age = 52
+
+
+print(copyOfClassJames.age)
+print(copyOfStructJames.age)
+
+print(classJames.age) //Reference type: beklemedigimiz sonucu veren.
+print(structJames.age) // Value type : bekledigimiz sonucu veren.
+
+//Reference Types -> Class
+//Copy -> Same object new reference -> 1 object + 2 Reference
+
+
+//Value Types -> Struct
+//Copy -> new object -> 2 objects
+
+
